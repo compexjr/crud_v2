cdf6e5e26e5b49d5f26e083ff38c4c7997162c3d
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2025-02-26 20:02:51 -0300

    :tada: Starting the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import TaskFilterBar from "@/components/task-filter-bar/page";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <TaskFilterBar />
+      </div>
+    </main>
+  );
+}
